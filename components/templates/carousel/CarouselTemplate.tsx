eb31f8f78864e8c3667a189a33417166827b923d
Santi Mansilla <santi755@gmail.com>
2022-10-21 22:04:12 +0200

    Carousel WIP

--- components/templates/carousel/CarouselTemplate.tsx
+++ components/templates/carousel/CarouselTemplate.tsx
@@ -1,8 +1,11 @@
-import styles from './CarouselTemplate.module.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper';
+import Image from 'next/image';
 
 import 'swiper/css';
-
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import styles from './CarouselTemplate.module.css';
 export interface ICarouselTemplate {
   sampleTextProp: string;
 }
@@ -10,15 +13,24 @@ export interface ICarouselTemplate {
 const CarouselTemplate: React.FC<ICarouselTemplate> = ({ sampleTextProp }) => {
   return (
     <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
+      className={styles.carousel}
+      modules={[Navigation, Pagination]}
+      spaceBetween={0}
+      slidesPerView={1}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
+      navigation
+      pagination={{ clickable: true }}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>
+        <Image src="/assets/images/carousel/serie1-3840x2160.jpg" alt="HBO MAX" width="3840" height="2160" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src="/assets/images/carousel/serie2-3840x2160.jpg" alt="HBO MAX" width="3840" height="2160" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src="/assets/images/carousel/serie3-3840x2160.jpg" alt="HBO MAX" width="3840" height="2160" />
+      </SwiperSlide>
     </Swiper>
   );
 };
